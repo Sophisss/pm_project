e1ce4d1375198f65852394db67683fa7e50a7747
Sofia Scattolini <sofia.scattolini@studenti.unicam.it>
2025-06-06 17:19:07 +0200

    conformance checking: token based replay

--- scripts/alpha_miner.py
+++ scripts/alpha_miner.py
@@ -1,10 +1,7 @@
 from import_xes import import_xes
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
-from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
-from pm4py.algo.evaluation.replay_fitness.variants import token_replay as fitness_evaluator
-from pm4py.algo.evaluation.precision import algorithm as precision_evaluator
-from pm4py.algo.conformance.alignments.petri_net import algorithm as alignments
+from token_based_replay import perform_token_based_replay
 
 def run_alpha_miner(log):
     # Discover and visualize the Petri Net
@@ -13,35 +10,7 @@ def run_alpha_miner(log):
     pn_visualizer.view(gviz)
 
     # Conformance checking
-    _perform_token_based_replay(log, net, initial_marking, final_marking)
-    # _perform_alignment(log, net, initial_marking, final_marking)
-
-def _perform_token_based_replay(log, net, initial_marking, final_marking):
-    # Perform token-based replay
-    tbr_result = token_replay.apply(log, net, initial_marking, final_marking)
-    # Calculate the fitness
-    token_replay_fitness = _evaluate_fitness(tbr_result)
-    print("Token-Based Replay Fitness:", token_replay_fitness)
-    # Calculate the precision
-    token_replay_precision = _evaluate_precision(log, net, initial_marking, final_marking)
-    print("Token-Based Replay Precision:", token_replay_precision)
-
-def _evaluate_fitness(tbr_result):
-    fitness = fitness_evaluator.evaluate(tbr_result)
-    return fitness
-
-def _evaluate_precision(log, net, initial_marking, final_marking):
-    precision = precision_evaluator.apply(log, net, initial_marking, final_marking)
-    return precision
-
-def _perform_alignment(log, net, initial_marking, final_marking):
-    alignment = alignments.apply(log, net, initial_marking, final_marking)
-    print("alignment:", alignment)
-    alignment_fitness = _evaluate_fitness(alignment)
-    print("Alignment Fitness:", alignment_fitness)
-    precision_alignment = _evaluate_precision(log, net, initial_marking, final_marking)
-    print("Precision Alignment Fitness:", precision_alignment)
-
+    perform_token_based_replay(log, net, initial_marking, final_marking)
 
 if __name__ == "__main__":
     event_log = import_xes()
--- scripts/heuristic_miner.py
+++ scripts/heuristic_miner.py
@@ -1,43 +1,22 @@
-import pm4py
-import pm4py.objects.conversion.heuristics_net.variants.to_petri_net as hn_to_petri_converter
+from import_xes import import_xes
 from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
 from pm4py.visualization.heuristics_net import visualizer as hn_visualizer
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
-from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
-from pm4py.algo.evaluation.replay_fitness.variants import token_replay as fitness_evaluator
-from pm4py.algo.evaluation.precision import algorithm as precision_evaluator
-from import_xes import import_xes
+from token_based_replay import perform_token_based_replay
 
 def run_heuristic_miner(log):
     # Discover and visualize the Heuristic Net
     heu_net = heuristics_miner.apply_heu(log)
-
     gviz = hn_visualizer.apply(heu_net)
     hn_visualizer.view(gviz)
 
-    # Convert the Heuristic Net into a Petri Net (needed for token-based replay)
-    net, initial_marking, final_marking = hn_to_petri_converter.apply(heu_net)
+    # Convert the Heuristic Net into a Petri Net
+    net, initial_marking, final_marking = heuristics_miner.apply(event_log)
     gviz = pn_visualizer.apply(net, initial_marking, final_marking)
     pn_visualizer.view(gviz)
 
-    # Perform token-based replay for conformance checking
-    tbr_result = token_replay.apply(log, net, initial_marking, final_marking)
-    # Calculate the fitness
-    _evaluate_fitness(tbr_result)
-    # Calculate the precision
-    _evaluate_precision(log, net, initial_marking, final_marking)
-
-
-def _evaluate_fitness(tbr_result):
-    fitness = fitness_evaluator.evaluate(tbr_result)
-    print("Heuristic Miner Fitness:", fitness)
-    return fitness
-
-def _evaluate_precision(log, net, initial_marking, final_marking):
-    precision = precision_evaluator.apply(log, net, initial_marking, final_marking)
-    print("Heuristic Miner Precision:", precision)
-    return precision
-
+    # Conformance checking
+    perform_token_based_replay(log, net, initial_marking, final_marking)
 
 if __name__ == "__main__":
     event_log = import_xes()
