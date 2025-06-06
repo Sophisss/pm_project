3c2e024318b545f7cc4e31d6ae7bed85fb2d4db2
Sofia Scattolini <sofia.scattolini@studenti.unicam.it>
2025-06-06 17:18:52 +0200

    Updated import xes file

--- scripts/import_xes.py
+++ scripts/import_xes.py
@@ -1,15 +1,19 @@
-import pm4py
+from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py import get_start_activities, get_end_activities
 
 def import_xes():
-    log = pm4py.read_xes("../dataset/Road_Traffic_Fine_Management_Process.xes")
-    num_events = len(log)
+    log = xes_importer.apply("../dataset/Road_Traffic_Fine_Management_Process.xes")
+    num_traces = len(log)
+    num_events = sum(len(trace) for trace in log)
     start_activities = get_start_activities(log)
     end_activities = get_end_activities(log)
+    activities = set(event["concept:name"] for trace in log for event in trace)
+    num_activities = len(activities)
     print(
-        "Number of events: {}\nNumber of start activities: {}\nStart activities: {}\nNumber of end activities: {}\nEnd activities: {}".format(
-            num_events, len(start_activities), start_activities,
-            len(end_activities), end_activities))
+        "Number of traces: {}\nNumber of events: {}\nNumber of start activities: {}\nStart activities: {}\nNumber of end activities: {}\nEnd activities: {}\n"
+        "Number of unique activities: {}".format(
+            num_traces, num_events, len(start_activities), start_activities,
+            len(end_activities), end_activities, num_activities))
     return log
 
 if __name__ == "__main__":
